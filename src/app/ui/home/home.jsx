9b4e3ed1b3aefa07ddeca4fa14e57baf862e8a2b
diegoseg_15 <diegofersv@gmail.com>
2024-01-29 20:08:55 -0500

    Color de borde de las historias segun si es mejor amigo, sin ver o visto

--- src/app/ui/home/home.jsx
+++ src/app/ui/home/home.jsx
@@ -9,71 +9,104 @@ export default function HomePage() {
       userName: "sarahval1514",
       userAvatar:
         "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 3,
+      view: 2,
+      bestfriend: [2, 3],
     },
     {
       id: 2,
       userName: "alexsan15",
       userAvatar:
         "https://images.pexels.com/photos/428361/pexels-photo-428361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 4,
+      view: 1,
+      bestfriend: [2, 3],
     },
     {
       id: 3,
       userName: "mafersh4",
       userAvatar:
         "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 3,
+      view: 3,
+      bestfriend: [2, 3],
     },
     {
       id: 4,
       userName: "mario_rbyhjhjhkjkjkjkjkjkj",
       userAvatar:
         "https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 6,
+      view: 0,
+      bestfriend: [1, 4],
     },
     {
       id: 5,
       userName: "sergio-stp15",
       // userAvatar:
       //   "https://images.pexels.com/photos/1906157/pexels-photo-1906157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 3,
+      view: 2,
+      bestfriend: [2, 3],
     },
     {
       id: 6,
       userName: "carlostapiasm",
       userAvatar:
         "https://images.pexels.com/photos/733767/pexels-photo-733767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 3,
+      view: 0,
+      bestfriend: [1],
     },
     {
       id: 7,
       userName: "sam_flores15",
       userAvatar:
         "https://images.pexels.com/photos/1194806/pexels-photo-1194806.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 3,
+      view: 0,
+      bestfriend: [2, 3],
     },
     {
       id: 8,
       userName: "taylor-12",
       userAvatar:
         "https://images.pexels.com/photos/4255392/pexels-photo-4255392.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 3,
+      view: 0,
+      bestfriend: [2, 3],
     },
     {
       id: 9,
       userName: "danny80sd",
       userAvatar:
         "https://images.pexels.com/photos/16373346/pexels-photo-16373346/free-photo-of-moda-mujer-camisa-flor.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 5,
+      view: 5,
+      bestfriend: [2, 3],
     },
     {
       id: 10,
       userName: "smpay97",
       userAvatar:
         "https://images.pexels.com/photos/19601923/pexels-photo-19601923/free-photo-of-moda-mujer-verano-jardin.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      quantity: 3,
+      view: 3,
+      bestfriend: [2, 3],
     },
   ];
   return (
     <>
-      <div className="flex sm:max-w-[620px] md:w-full w-screen sm:mt-5 mt-14 overflow-x-auto px-2 mx-auto">
+      <div className="flex sm:max-w-[620px] md:w-full w-screen sm:mt-5 mt-14 overflow-x-auto px-2 sm:mx-auto">
         {storiesData.map((storyData) => (
           <span key={storyData.id}>
             <Suspense fallback={<StoriesHomeSkeletons />}>
               <StoriesHome
                 userAvatar={storyData.userAvatar}
                 userName={storyData.userName}
+                quantity={storyData.quantity}
+                view={storyData.view}
+                bestfriend={storyData.bestfriend}
               />
             </Suspense>
           </span>
--- src/app/ui/home/stories.jsx
+++ src/app/ui/home/stories.jsx
@@ -2,25 +2,33 @@ import React from "react";
 import Image from "next/image";
 
 export default function StoriesHome(props) {
-  const { userAvatar, userName } = props;
+  const { userAvatar, userName, quantity, view, bestfriend } = props;
 
   return (
     // <div className="flex flex-row overflow-x-auto max-w-[620px] w-full space-x-1">
-    <button className="flex max-w-20 flex-col p-2 items-center overflow-hidden">
-      <div className="relative w-14 h-14  rounded-full overflow-hidden flex items-center justify-center">
-        <Image
-          className={
-            userAvatar ? "absolute md:max-w-24 md:min-w-14 max-w-32" : "w-14"
-          }
-          src={
-            userAvatar
-              ? userAvatar.toString()
-              : "https://scontent-ams4-1.cdninstagram.com/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=1&_nc_ohc=vxSS1aHWde8AX-Z1B_W&edm=AAAAAAABAAAA&ccb=7-5&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2-ccb7-5&oh=00_AfBJGtVq8pu9dnnh06I81IgTKfl7ezHEoflnQkRE-bZnog&oe=65BC500F&_nc_sid=2c5659"
-          }
-          alt="historia"
-          width={300}
-          height={300}
-        />
+    <button className="flex max-w-20 flex-col items-center p-1.5">
+      <div
+        className={`${
+          quantity === view
+            ? "bg-zinc-200"
+            : bestfriend.find((best) => best === view)
+            ? "bg-green-500"
+            : "bg-gradient-to-tr via-pink-500 to-purple-500 from-yellow-400"
+        } p-0.5 rounded-full flex items-center justify-center overflow-hidden`}
+      >
+        <div className="relative w-14 h-14 border-3 border-white rounded-full overflow-hidden flex items-center justify-center">
+          <Image
+            className={userAvatar ? "absolute max-w-24 min-w-14 " : "max-w-16"}
+            src={
+              userAvatar
+                ? userAvatar.toString()
+                : "https://scontent-ams4-1.cdninstagram.com/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=1&_nc_ohc=vxSS1aHWde8AX-Z1B_W&edm=AAAAAAABAAAA&ccb=7-5&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2-ccb7-5&oh=00_AfBJGtVq8pu9dnnh06I81IgTKfl7ezHEoflnQkRE-bZnog&oe=65BC500F&_nc_sid=2c5659"
+            }
+            alt="historia"
+            width={300}
+            height={300}
+          />
+        </div>
       </div>
       <span className="w-full py-3 text-black text-xs overflow-hidden text-ellipsis text-nowrap">
         {userName}
